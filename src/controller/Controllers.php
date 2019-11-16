e95e482a54eec351c2e0646a3cc074a100ff5f13
mrnobody2002 <filipemelo032@gmail.com>
2019-11-15 20:06:17 -0500

    Headers set

--- src/controller/Controllers.php
+++ src/controller/Controllers.php
@@ -33,19 +33,21 @@ class Controllers{
         $this->proxies = explode("\r\n",curl_exec($this->chAuth));
     }*/
     protected function cURL_Setup($set){
-        curl_setopt($set, CURLOPT_USERAGENT, "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0");
-        curl_setopt($set, CURLOPT_HTTPHEADER,
-            array(
-                "Accept-Language: pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3",
-                "Content-Type: application/x-www-form-urlencoded",
-                 "DNT: 1",
-                "Sec-Fetch-Site: same-origin",
-                "Sec-Fetch-Mode: cors",
-                "request-type: 2",
-                "Host: www.siepe.educacao.pe.gov.br",
-                "Connection: keep-alive",
-                "Referer: http://www.siepe.educacao.pe.gov.br/"
-                  ));
+        curl_setopt($set, CURLOPT_ENCODING, 'gzip, deflate');
+
+        $headers = array();
+        $headers[] = 'Host: www.siepe.educacao.pe.gov.br';
+        $headers[] = 'Origin: https://www.siepe.educacao.pe.gov.br';
+        $headers[] = 'X-Requested-With: XMLHttpRequest';
+        $headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36';
+        $headers[] = 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8';
+        $headers[] = 'Accept: */*';
+        $headers[] = 'Sec-Fetch-Site: same-origin';
+        $headers[] = 'Sec-Fetch-Mode: cors';
+        $headers[] = 'Referer: https://www.siepe.educacao.pe.gov.br/';
+        $headers[] = 'Accept-Language: pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7';
+        curl_setopt($set, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($set, CURLOPT_RETURNTRANSFER, true);
 
         curl_setopt($set, CURLOPT_COOKIEJAR, $this->cookies);
         curl_setopt($set, CURLOPT_COOKIEFILE,$this->cookies);
