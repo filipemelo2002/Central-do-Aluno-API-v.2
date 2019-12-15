30f5848beba91355f762e601b8fc58cd0b66b0b1
mrnobody2002 <filipemelo032@gmail.com>
2019-12-15 07:58:47 -0300

    CORS allowed

--- src/routes/index.php
+++ src/routes/index.php
@@ -22,6 +22,25 @@ $app->get('/faltas',FaltasController::class .':index');
 $app->get('/horarios', HorarioController::class.':index');
 $app->post('/admin/login', AdminController::class.':login');
 $app->get('/admin/view', AdminController::class.':getData');
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
+
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+    $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+    return $handler($req, $res);
+});
+
 $app->run();
 
 /*RewriteEngine On
